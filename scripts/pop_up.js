28247db6e8271f7dc84a6fef408583aca70ca32f
Nikolay <niksan9779@abv.bg>
2019-06-23 22:09:11 +0300

    first commit

---
+++ scripts/pop_up.js
@@ -0,0 +1,17 @@
+// Get the modal
+var register = document.getElementById('register');
+
+// When the user clicks anywhere outside of the modal, close it
+
+// Get the modal
+var login = document.getElementById('login');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == login) {
+        login.style.display = "none";
+    }
+    if (event.target == register) {
+        register.style.display = "none";
+    }
+}
